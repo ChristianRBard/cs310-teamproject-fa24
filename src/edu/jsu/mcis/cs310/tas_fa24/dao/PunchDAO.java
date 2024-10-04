323563a3361cce9d8db8d7270c7aae35bb8edc6d
Caden Parrish <cparrish4@stu.jsu.edu>
2024-10-04 12:13:35 -0500

    Made constructor getters and setters for punch class

--- src/edu/jsu/mcis/cs310/tas_fa24/dao/PunchDAO.java
+++ src/edu/jsu/mcis/cs310/tas_fa24/dao/PunchDAO.java
@@ -1,5 +1,16 @@
 package edu.jsu.mcis.cs310.tas_fa24.dao;
+import edu.jsu.mcis.cs310.tas_fa24.Punch;
 
 public class PunchDAO {
+    private static final String Query_Find = "SELECT * FROM event WHERE id = ?";
     
+    private final DAOFactory daoFactory;
+    
+    PunchDAO(DAOFactory daoFactory){
+        this.daoFactory = daoFactory;
+    }
+    
+    public Punch find(int terminalid, Badge badge, EventType punchtype){
+        
+    }
 }
