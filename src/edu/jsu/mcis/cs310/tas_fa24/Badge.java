3b7266f783f8d6204006f2daf4fd476a540a1283
Connor Bain <jcbain33@gmail.com>
2024-12-02 11:15:09 -0600

    Added missing javadoc

--- src/edu/jsu/mcis/cs310/tas_fa24/Badge.java
+++ src/edu/jsu/mcis/cs310/tas_fa24/Badge.java
@@ -1,7 +1,16 @@
 package edu.jsu.mcis.cs310.tas_fa24;
 
+/**
+ * <p>This class provides the necessary methods to create and access a Badge object</p>
+ * @author Jay Snellen
+ */
 public class Badge {
 
+    /**
+     * <p>These are the necessary attributes of a Badge object</P>
+     * @param id This is the unique id that every badge in the database has
+     * @param description This stores the name of who the badge belongs to
+     */
     private final String id, description;
 
     public Badge(String id, String description) {
--- src/edu/jsu/mcis/cs310/tas_fa24/EventType.java
+++ src/edu/jsu/mcis/cs310/tas_fa24/EventType.java
@@ -1,7 +1,12 @@
 package edu.jsu.mcis.cs310.tas_fa24;
-
+/**
+ * <p>This class creates an object to store the type of punch that was made</p>
+ * @author Jay Snellen
+ */
 public enum EventType {
-
+    /**
+     * <p>The necessary types of punches that can be made</p>
+     */
     CLOCK_OUT("CLOCK OUT"),
     CLOCK_IN("CLOCK IN"),
     TIME_OUT("TIME OUT");
